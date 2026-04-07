44d7aedffc703bd06452514ecc94b3c06956a05a
dudu-theman <dustanley07@gmail.com>
2026-04-07 13:05:18 -0500

    make moral_outrage_score int instead of float

--- schemas/responses.py
+++ schemas/responses.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel
 class MoralOutrage(BaseModel):
     text_id: str
     text: str
-    moral_outrage_score: float
+    moral_outrage_score: int
     label_timestamp: str
