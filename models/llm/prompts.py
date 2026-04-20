19e87b038be10e9a9689a3b9a9536f0d6cb02e1a
mark-torres10 <markptorres1@gmail.com>
2026-04-20 12:17:42 -0500

    Update prompt

--- models/llm/prompts.py
+++ models/llm/prompts.py
@@ -4,17 +4,13 @@ You are a helpful assistant. Your job is to analyze a single social media post a
 
 The key definition of moral outrage included the following three components: a person can be viewed as expressing moral outrage if (1) they have feelings in response to a perceived violation of their personal morals, (2) their feelings are comprised of emotions such as anger, disgust and contempt, and (3) the feelings are associated with specific reactions including blaming people/events/things, holding them responsible, or wanting to punish them.
 
-Moral outrage is a emotional response associated with a host of reactions. Most of us just “know” what outrage is based on our experience. But to be more precise, we can say someone is morally outraged when:
+Moral outrage is a emotional response associated with a host of reactions. Most of us just "know" what outrage is based on our experience. But to be more precise, we can say someone is morally outraged when:
 
-(1) They have feelings in response to a violation of their morals. For instance, Bob thinks that abortion is morally wrong, and when his city passed a law that legalized abortion he became very upset. Since the law upset him because it violated
-his moral views about abortion, the feeling he felt might be moral outrage.
+(1) They have feelings in response to a violation of their morals. For instance, Bob thinks that abortion is morally wrong, and when his city passed a law that legalized abortion he became very upset. Since the law upset him because it violated his moral views about abortion, the feeling he felt might be moral outrage.
 
-(2) Their feelings are comprised of emotions such as anger, disgust and contempt. For instance, Bob was upset but specifically he was angered and disgusted at the fact that abortion would become legal in his city. These emotions made him feel very negative and also “worked up”.
+(2) Their feelings are comprised of emotions such as anger, disgust and contempt. For instance, Bob was upset but specifically he was angered and disgusted at the fact that abortion would become legal in his city. These emotions made him feel very negative and also "worked up".
 
-(3) They are associated with specific reactions: blaming people/events/things, holding them responsible, or wanting to punish them. For instance, Bob blamed all the non-religious voters in his city for allowing the law to
-pass. He also felt like the city council members should be held responsible for allowing
-the law to actually become reality. For all these reasons, we can say Bob is morally outraged about the legalizing of abortion
-in his city.
+(3) They are associated with specific reactions: blaming people/events/things, holding them responsible, or wanting to punish them. For instance, Bob blamed all the non-religious voters in his city for allowing the law to pass. He also felt like the city council members should be held responsible for allowing the law to actually become reality. For all these reasons, we can say Bob is morally outraged about the legalizing of abortion in his city.
 
 - If you judge that the post describes, reports, or implies moral outrage, respond with: "1"
 - If the post is unrelated or does not describe moral outrage, respond with: "0"
